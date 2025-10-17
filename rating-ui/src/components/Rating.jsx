36f1ed8918674300f2c1d31f5b351f37e14ab9e2
semicolon guy <itworksnavneet@gmail.com>
2025-10-17 18:59:20 +0530

    mvp

---
+++ rating-ui/src/components/Rating.jsx
@@ -0,0 +1,12 @@
+import Stars from "./Stars";
+
+const Rating = () => {
+  return (
+    <div className="rating-container">
+      <h2>Rate your experience!</h2>
+      <Stars />
+    </div>
+  );
+};
+
+export default Rating;
